18ad35c2542e98be85d6b07888a4e377476d33b2
Medboubazine <mohamedtorino161@gmail.com>
2024-05-24 11:41:58 +0100

    First

---
+++ src/ServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Medboubazine\LaravelReportTool;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+final class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Bootstrap package services.
+     */
+    public function boot(): void
+    {
+    }
+}
